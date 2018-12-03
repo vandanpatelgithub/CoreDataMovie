d82c8f5a7f469d5b0a58538ac4e26299ee5310f1
Vandan Patel <vpatel@vandans-macbook-pro.local>
2018-12-02 20:29:49 -0800

    Initial Commit

---
+++ CoreDataMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataMovie
+//
+//  Created by Vandan Patel on 12/2/18.
+//  Copyright © 2018 Vandan Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
